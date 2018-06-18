0c9ca5f6834cd81f77add11f91a15e62a895a718
Matija Horvat <horvat2112@gmail.com>
2018-06-18 15:16:40 +0200

    add SetUpEditor function in ScintillaCtrl implement SetUpEditor function

--- NWPEditor/ScintillaCtrl.cpp
+++ NWPEditor/ScintillaCtrl.cpp
@@ -15,6 +15,17 @@ LRESULT ScintillaCtrl::SendEditor(int msg, WPARAM wparam, LPARAM lparam) {
 	return ::SendMessage(scintillaCtrl, msg, wparam, lparam);
 }
 
+void ScintillaCtrl::SetAStyle(int style, COLORREF fore, COLORREF back, int size, const char *face)
+{
+	SendEditor(SCI_STYLESETFORE, style, fore);
+	SendEditor(SCI_STYLESETBACK, style, back);
+
+	if (size >= 1)
+		SendEditor(SCI_STYLESETSIZE, style, size);
+	else
+		SendEditor(SCI_STYLESETFONT, style, reinterpret_cast<LPARAM>(face));
+}
+
 void ScintillaCtrl::SetLang(int lex, bool clang) {
 	switch (lex) {
 	case SCLEX_CPP:
@@ -33,5 +44,29 @@ void ScintillaCtrl::SetLang(int lex, bool clang) {
 		SendEditor(SCI_SETLEXER, SCLEX_NULL);
 	}
 
+	SetUpEditor();
+}
+
+void ScintillaCtrl::SetUpEditor()
+{
+	SendEditor(SCI_SETTABWIDTH, 4);
 	SendEditor(SCI_SETKEYWORDS, NULL, reinterpret_cast<LPARAM>(ini.GetKeywords()));
+
+	//Default Style
+	SetAStyle(STYLE_DEFAULT, RGB(255,255,255), RGB(56, 57, 58), 10, "Arial");
+
+	SendEditor(SCI_SETCARETFORE, RGB(255, 255, 255));
+	SendEditor(SCI_STYLECLEARALL, NULL);
+	SendEditor(SCI_SETSELBACK, TRUE, RGB(0, 0, 255));
+
+	SetAStyle(SCE_C_COMMENT, ini.GetColor(_T("green")));
+	SetAStyle(SCE_C_COMMENTLINE, ini.GetColor(_T("green")));
+	SetAStyle(SCE_C_COMMENTDOC, ini.GetColor(_T("green")));
+	SetAStyle(SCE_C_NUMBER, ini.GetColor(_T("magenta")));
+	SetAStyle(SCE_C_STRING, ini.GetColor(_T("yellow")));
+	SetAStyle(SCE_C_CHARACTER, ini.GetColor(_T("yellow")));
+	SetAStyle(SCE_C_UUID, ini.GetColor(_T("cyan")));
+	SetAStyle(SCE_C_OPERATOR, ini.GetColor(_T("red")));
+	SetAStyle(SCE_C_PREPROCESSOR, ini.GetColor(_T("red")));
+	SetAStyle(SCE_C_WORD, ini.GetColor(_T("red")));
 }
--- NWPEditor/ScintillaCtrl.h
+++ NWPEditor/ScintillaCtrl.h
@@ -13,7 +13,9 @@ protected:
 	IniCtrl ini;
 	Color clr;
 protected:
-
+	void SetUpEditor();
+	LRESULT SendEditor(int msg, WPARAM wparam, LPARAM lparam = NULL);
+	void SetAStyle(int style, COLORREF fore, COLORREF back=RGB(56,57,58), int size=NULL, const char *face=NULL);
 public:
 	//construction and deconstruction
 	ScintillaCtrl();
@@ -23,5 +25,4 @@ public:
 	void SetScintillaCtrl(HWND wnd);
 	void SetLang(int langm, bool clang=false);
 	HWND GetScintillaCtrl();
-	LRESULT SendEditor(int msg, WPARAM wparam, LPARAM lparam=NULL);
 };
