5410f97d33221641c0e18a566878af8f7216b080
Matija Horvat <horvat2112@gmail.com>
2018-07-20 10:00:13 +0200

    fix keyword coloring

--- NWPEditor/ScintillaCtrl.cpp
+++ NWPEditor/ScintillaCtrl.cpp
@@ -2,6 +2,16 @@
 #include "stdafx.h"
 #include "ChildView.h"
 
+char keywords[] = "asm auto bool break case catch char class const "
+"dynamic_cast else enum explicit extern false finally "
+"float for friend goto if inline int long mutable "
+"namespace new operator private protected public "
+"register reinterpret_cast register return short signed "
+"sizeof static static_cast struct switch template "
+"this throw true try typedef typeid typename "
+"union unsigned using virtual void volatile "
+"wchar_t while";
+
 //construction and deconstruction
 ScintillaCtrl::ScintillaCtrl() {}
 
@@ -50,7 +60,9 @@ void ScintillaCtrl::SetLang(int lex, bool clang/*=false*/)
 
 void ScintillaCtrl::SetUpEditor()
 {
-	SendEditor(SCI_SETKEYWORDS, NULL, reinterpret_cast<LPARAM>(ini.GetKeywords()));
+	//TODO: Change types of all keywords
+	//SendEditor(SCI_SETKEYWORDS, NULL, reinterpret_cast<LPARAM>(ini.GetKeywords()));
+	SendEditor(SCI_SETKEYWORDS, NULL, (LPARAM)keywords);
 
 	SetAStyle(SCE_C_COMMENT, ini.GetColor(_T("comment")));
 	SetAStyle(SCE_C_COMMENTLINE, ini.GetColor(_T("comment")));
