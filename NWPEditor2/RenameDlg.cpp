9b5210bf9440180c1d21853c565d5bed47c8bdce
Matija Horvat <horvat2112@gmail.com>
2019-05-25 14:38:27 +0200

    add RenameDlgFiles

---
+++ NWPEditor2/RenameDlg.cpp
@@ -0,0 +1,73 @@
+// RenameDlg.cpp : implementation file
+//
+#pragma once
+
+#include "stdafx.h"
+#include "resource.h"
+#include "RenameDlg.h"
+#include "afxdialogex.h"
+
+// RenameDlg dialog
+
+IMPLEMENT_DYNAMIC(RenameDlg, CDialogEx)
+
+RenameDlg::RenameDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_RENAME_DIALOG, pParent)
+{
+}
+
+RenameDlg::~RenameDlg()
+{
+}
+
+void RenameDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDITBOX_RENAMEFROM, m_rename_from_eb);
+	DDX_Control(pDX, IDC_EDITBOX_RENAMETO, m_rename_to_eb);
+}
+
+BEGIN_MESSAGE_MAP(RenameDlg, CDialogEx)
+	ON_BN_CLICKED(IDOK, &RenameDlg::OnBtnClickedOk)
+	ON_BN_CLICKED(IDCANCEL, &RenameDlg::OnBtnClickedCancel)
+END_MESSAGE_MAP()
+
+// RenameDlg message handlers
+
+void RenameDlg::OnBtnClickedOk()
+{
+	m_rename_to_eb.GetWindowTextW(m_rename_to_string);
+	m_rename_from_eb.GetWindowTextW(m_rename_from_string);
+
+	if (CheckRenameCondition())
+	{
+		CString msg, title;
+		msg.LoadStringW(IDS_RENAMEDLG_MSG);
+		title.LoadStringW(IDS_RENAMEDLG_TITLE);
+		MessageBox(msg, title, MB_OK | MB_ICONWARNING);
+
+		return;
+	}
+
+	CDialogEx::OnOK();
+}
+
+void RenameDlg::OnBtnClickedCancel()
+{
+	CDialogEx::OnCancel();
+}
+
+CString RenameDlg::GetRenameTo()const
+{
+	return m_rename_to_string;
+}
+
+CString RenameDlg::GetRenameFrom()const
+{
+	return m_rename_from_string;
+}
+
+bool RenameDlg::CheckRenameCondition()const
+{
+	return m_rename_to_string.IsEmpty() || m_rename_from_string.IsEmpty();
+}
