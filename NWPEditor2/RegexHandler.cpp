77496f0dac374bad5fc74f10b84acd6d7957f610
Matija Horvat <Matija.Horvat@enghouse.com>
2019-06-03 16:58:49 +0200

    add new methods in RegexHandler

--- NWPEditor2/RegexHandler.cpp
+++ NWPEditor2/RegexHandler.cpp
@@ -6,10 +6,47 @@ RegexHandler::RegexHandler(int lang, std::string line)
 	ParseRegex(lang, line);
 }
 
+bool RegexHandler::CheckIfVariable(std::string line, std::regex lang_regex)
+{
+	return true;
+}
+
+bool RegexHandler::CheckIfFunction(std::string line, std::regex lang_regex)
+{
+	return true;
+}
+
+bool RegexHandler::CheckLineMatch(std::string line)
+{
+	return true;
+}
+
+std::string RegexHandler::ExtractFunctionName(int lang, std::string line)
+{
+	switch (lang) {
+	case SCLEX_CPP:
+	{
+		std::string::iterator iter;
+		if (line.find(':') != std::string::npos)
+		{
+			iter = std::find(line.begin(), line.end(), ':');
+			return ExtractCTypeName(iter + ITERATOR_CLANG_OFFSET);
+		}
+	}
+	case SCLEX_PYTHON:
+	default:
+		return "";
+	}
+}
+
 void RegexHandler::ParseRegex(int lang, std::string line)
 {
 	switch (lang) {
 	case SCLEX_CPP:
+		if (CheckIfFunction(line, std::regex("*(*::*)?\(\)*\n?*\{*\}")))
+		{
+			regex_in_use = std::regex("");
+		}
 		break;
 	case SCLEX_PYTHON:
 		break;
--- NWPEditor2/RegexHandler.h
+++ NWPEditor2/RegexHandler.h
@@ -2,15 +2,19 @@
 #include "string"
 #include "regex"
 #include "exception"
+#include "algorithm"
+
+#define ITERATOR_CLANG_OFFSET 2
 
 class RegexHandler
 {
 	std::regex regex_in_use;
 protected:
 	void ParseRegex(int lang, std::string line);
-	bool ParseLine();
 	bool CheckIfVariable(std::string line, std::regex lang_regex);
 	bool CheckIfFunction(std::string line, std::regex lang_regex);
+	std::string ExtractFunctionName(int lang, std::string line);
+	std::string ExtractCTypeName(std::string::iterator string_iterator);
 public:
 	RegexHandler(int lang, std::string line);
 	bool CheckLineMatch(std::string line);
