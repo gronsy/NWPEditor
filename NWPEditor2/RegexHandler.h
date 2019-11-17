dc3bda88831d1ef2659bdac7cff71fbc494a5f25
Matija Horvat <horvat2112@gmail.com>
2019-11-17 21:42:02 +0100

    gramatically correct

--- NWPEditor2/RegexHandler.h
+++ NWPEditor2/RegexHandler.h
@@ -12,7 +12,7 @@
 #define ITERATOR_CLANG_COLON_OFFSET 2
 #define ITERATOR_SPACE_OFFSET 1
 #define ERASE_OFFSET 1
-#define STRING_BEGINING 0
+#define STRING_BEGINNING 0
 
 public ref class RegexHandler
 {
